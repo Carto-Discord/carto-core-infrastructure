e46db170f63e6bacb8bb0d04a1b316c525b00f54
Miles Bardon <milesbardon@gmail.com>
2021-04-17 22:21:24 +0100

    Changing bindings to members

--- iam.tf
+++ iam.tf
@@ -11,65 +11,44 @@ output "terraform_private_key" {
   value = google_service_account_key.terraform_key.private_key
 }
 
-resource "google_project_iam_binding" "appengine_binding" {
+resource "google_project_iam_member" "appengine_binding" {
   project = var.project_id
   role    = "roles/appengine.deployer"
-
-  members = [
-    "serviceAccount:${google_service_account.terraform_sa.email}",
-  ]
+  member  = "serviceAccount:${google_service_account.terraform_sa.email}"
 }
 
-resource "google_project_iam_binding" "cloudbuild_binding" {
+resource "google_project_iam_member" "cloudbuild_binding" {
   project = var.project_id
   role    = "roles/cloudbuild.builds.builder"
-
-  members = [
-    "serviceAccount:${google_service_account.terraform_sa.email}",
-  ]
+  member  = "serviceAccount:${google_service_account.terraform_sa.email}"
 }
 
-resource "google_project_iam_binding" "cloudscheduler_binding" {
+resource "google_project_iam_member" "cloudscheduler_binding" {
   project = var.project_id
   role    = "roles/cloudscheduler.admin"
-
-  members = [
-    "serviceAccount:${google_service_account.terraform_sa.email}",
-  ]
+  member  = "serviceAccount:${google_service_account.terraform_sa.email}"
 }
 
-resource "google_project_iam_binding" "cloudstorage_binding" {
+resource "google_project_iam_member" "cloudstorage_binding" {
   project = var.project_id
   role    = "roles/storage.admin"
-
-  members = [
-    "serviceAccount:${google_service_account.terraform_sa.email}",
-  ]
+  member  = "serviceAccount:${google_service_account.terraform_sa.email}"
 }
 
-resource "google_project_iam_binding" "cloudfunctions_binding" {
+resource "google_project_iam_member" "cloudfunctions_binding" {
   project = var.project_id
-  role    = "roles/cloudfunctions.developer"
-
-  members = [
-    "serviceAccount:${google_service_account.terraform_sa.email}",
-  ]
+  role    = "roles/cloudfunctions.admin"
+  member  = "serviceAccount:${google_service_account.terraform_sa.email}"
 }
 
-resource "google_project_iam_binding" "compute_binding" {
+resource "google_project_iam_member" "compute_binding" {
   project = var.project_id
   role    = "roles/compute.viewer"
-
-  members = [
-    "serviceAccount:${google_service_account.terraform_sa.email}",
-  ]
+  member  = "serviceAccount:${google_service_account.terraform_sa.email}"
 }
 
-resource "google_project_iam_binding" "iam_binding" {
+resource "google_project_iam_member" "iam_binding" {
   project = var.project_id
   role    = "roles/iam.serviceAccountUser"
-
-  members = [
-    "serviceAccount:${google_service_account.terraform_sa.email}",
-  ]
+  member  = "serviceAccount:${google_service_account.terraform_sa.email}"
 }
